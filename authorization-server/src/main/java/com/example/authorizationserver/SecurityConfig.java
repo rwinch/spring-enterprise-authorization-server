f6b744c9ac171307336508cb3b9acd62179f1254
Rob Winch <rwinch@users.noreply.github.com>
2023-05-12 16:08:37 -0500

    ImmutableJWKSet

--- authorization-server/src/main/java/com/example/authorizationserver/SecurityConfig.java
+++ authorization-server/src/main/java/com/example/authorizationserver/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.example.authorizationserver;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -16,6 +22,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 import javax.sql.DataSource;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 // https://docs.spring.io/spring-security/reference/servlet/architecture.html
 @Configuration
@@ -77,6 +85,19 @@ public class SecurityConfig {
 	@Bean
 	JdbcOAuth2AuthorizationService authorizationService(DataSource dataSource, RegisteredClientRepository clientRepository) {
 		return new JdbcOAuth2AuthorizationService(new JdbcTemplate(dataSource), clientRepository);
+	}
+
+	@Bean
+	JWKSource<SecurityContext> jwkSource(
+			@Value("${jwt.key.id}") String id,
+			@Value("${jwt.key.private}") RSAPrivateKey privateKey,
+			@Value("${jwt.key.public}") RSAPublicKey publicKey) {
+		RSAKey rsaKey = new RSAKey.Builder(publicKey)
+						.privateKey(privateKey)
+						.keyID(id)
+						.build();
+		JWKSet jwkSet = new JWKSet(rsaKey);
+		return new ImmutableJWKSet<>(jwkSet);
 	}
 
 }
